98605cefad888b84d5eab31ebcefa94d85665b6b
Mark Bates <mark@markbates.com>
2017-07-17 21:17:55 -0400

    added some logging

--- gwa.go
+++ gwa.go
@@ -9,15 +9,19 @@ import (
 	"github.com/gocraft/work"
 	"github.com/markbates/going/defaults"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 type Options struct {
 	*redis.Pool
+	Logger         Logger
 	Name           string
 	MaxConcurrency int
 }
 
-func New(opts Options) worker.Worker {
+var _ worker.Worker = &Adapter{}
+
+func New(opts Options) *Adapter {
 	ctx := context.Background()
 
 	opts.Name = defaults.String(opts.Name, "buffalo")
@@ -26,9 +30,17 @@ func New(opts Options) worker.Worker {
 	opts.MaxConcurrency = defaults.Int(opts.MaxConcurrency, 25)
 	pool := work.NewWorkerPool(struct{}{}, uint(opts.MaxConcurrency), opts.Name, opts.Pool)
 
+	if opts.Logger == nil {
+		l := logrus.New()
+		l.Level = logrus.InfoLevel
+		l.Formatter = &logrus.TextFormatter{}
+		opts.Logger = l
+	}
+
 	return &Adapter{
 		Enqueur: enqueuer,
 		Pool:    pool,
+		Logger:  opts.Logger,
 		ctx:     ctx,
 	}
 }
@@ -36,10 +48,12 @@ func New(opts Options) worker.Worker {
 type Adapter struct {
 	Enqueur *work.Enqueuer
 	Pool    *work.WorkerPool
+	Logger  Logger
 	ctx     context.Context
 }
 
 func (q *Adapter) Start(ctx context.Context) error {
+	q.Logger.Info("Starting gocraft/work Worker")
 	q.ctx = ctx
 	go func() {
 		select {
@@ -52,6 +66,7 @@ func (q *Adapter) Start(ctx context.Context) error {
 }
 
 func (q *Adapter) Stop() error {
+	q.Logger.Info("Stopping gocraft/work Worker")
 	q.Pool.Stop()
 	return nil
 }
@@ -64,17 +79,21 @@ func (q *Adapter) Register(name string, h worker.Handler) error {
 }
 
 func (q Adapter) Perform(job worker.Job) error {
+	q.Logger.Infof("Enqueuing job %s\n", job)
 	_, err := q.Enqueur.Enqueue(job.Handler, job.Args)
 	if err != nil {
+		q.Logger.Errorf("error enqueuing job %s", job)
 		return errors.WithStack(err)
 	}
 	return nil
 }
 
 func (q Adapter) PerformIn(job worker.Job, t time.Duration) error {
+	q.Logger.Infof("Enqueuing job %s\n", job)
 	d := int64(t / time.Second)
 	_, err := q.Enqueur.EnqueueIn(job.Handler, d, job.Args)
 	if err != nil {
+		q.Logger.Errorf("error enqueuing job %s", job)
 		return errors.WithStack(err)
 	}
 	return nil
